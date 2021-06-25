824cea91e3091c9df2f9a92b99c0c5c74302aa09
Tasha <tashaasng78ws@gmail.com>
2021-06-25 19:16:37 +0800

    initial commit
    basic program code all in one file yay

---
+++ artist_favouritism.py
@@ -0,0 +1,74 @@
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from clientcreds import user_id, client_id, client_secret
+
+SPOTIPY_USER_ID = user_id
+SPOTIPY_CLIENT_ID = client_id
+SPOTIPY_CLIENT_SECRET = client_secret
+SPOTIPY_REDIRECT_URL = "http://127.0.0.1:9090"
+SCOPES = "playlist-read-private,playlist-modify-private"
+user_owned_playlists_id = []   # list of user-owned playlist ids
+playlist_items_add = []  # list of track IDs to add into playlist
+
+# performing oauth for user
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URL,
+    scope=SCOPES
+))
+
+# player inputs for artist name
+artist_name = input("Enter the artist's name: ")
+playlists_offest = input("\nEnter the playlist position (integer) you want to start searching from: ")
+print(f"\nSearching through your playlists...")
+
+# creating playlist for user
+sp.user_playlist_create(
+    SPOTIPY_USER_ID,
+    f"{artist_name} Favouritism",
+    public=False,
+    collaborative=False,
+    description=f"sometimes you just gotta listen to {artist_name} and {artist_name} only"
+)
+# get id of this newly created playlist
+new_playlist = sp.current_user_playlists(limit=1)
+new_playlist_id = new_playlist['items'][0]['id']
+
+# get current user's OWNED playlists
+user_playlists = sp.current_user_playlists(offset=playlists_offest)
+for playlist in user_playlists['items']:
+    playlist_id = playlist['id']
+    playlist_name = playlist['name']
+    playlist_owner = playlist['owner']['id']    
+    if playlist_owner == SPOTIPY_USER_ID:
+        user_owned_playlists_id.append(playlist_id)
+print(f"\nPlaylists owned by {SPOTIPY_USER_ID}: {user_owned_playlists_id}")
+print(f"\nFetching tracks by {artist_name}.....")
+
+
+# get the tracks by particular artist in the user's OWNED playlists
+for id in user_owned_playlists_id:
+    user_playlists_items = sp.playlist_tracks(id)
+    for items in user_playlists_items['items']:
+        for artist in items['track']['artists']:
+            if artist['name'] == artist_name:
+                if items['track']['uri'] not in playlist_items_add:
+                    playlist_items_add.append(items['track']['uri'])
+                    print(".")
+    # to extend the limit of 100 tracks to read in a playlist
+    while user_playlists_items['next']:
+        user_playlists_items = sp.next(user_playlists_items)
+        for items in user_playlists_items['items']:
+            for artist in items['track']['artists']:
+                if artist['name'] == artist_name:
+                    if items['track']['uri'] not in playlist_items_add:
+                        playlist_items_add.append(items['track']['uri'])
+                        print(".")
+
+print(f"\nTracks to add in new playlist: {playlist_items_add}")
+
+# add the list of playlist_items_add
+sp.playlist_add_items(new_playlist_id, playlist_items_add)
+print(f"\nAll tracks in your playlists by {artist_name} have been added to a newly created playlist!\n")
---
+++ clientcreds.py
@@ -0,0 +1,3 @@
+user_id = "enter your spotify user ID here"
+client_id = "enter your app's client ID here"
+client_secret = "enter your app's client secret here"
