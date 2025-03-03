f59e3a0c4302c3c0283380d45476be8c8702dfd5
Tasha S. <62389170+tishttash@users.noreply.github.com>
2025-03-03 15:57:10 +0800

    Update clientcreds.py

--- clientcreds.py
+++ clientcreds.py
@@ -1,4 +1,6 @@
-# the following can be found in your spotify developer account
+# your user id is your spotify username
 user_id = "enter user id here"
+
+# the following can be found in your spotify developer account
 client_id = "enter client id here (string)"
 client_secret = "enter client secret here (string)"
